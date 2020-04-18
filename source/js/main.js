d71ce8b84674f24db1fc4e407c438fc4342b7b01
Tanya311 <55144291+Tanya311@users.noreply.github.com>
2020-04-18 20:29:57 +0300

    implementation of the drop-down menu on the mobile version

---
+++ source/js/main.js
@@ -0,0 +1,36 @@
+var KEY_CODE_ENT = 25;
+var navigation = document.querySelector('.navigation')
+var buttonMenu = navigation.querySelector('.navigation__toggle');
+var navigationWrapper = navigation.querySelector('.navigation__wrapper');
+
+navigation.classList.remove('navigation--nojs');
+
+/**
+ * @name changeNavigationClass
+ * @description функция переключения класса навигации
+ */
+var changeNavigationClass = function () {
+  navigation.classList.toggle('navigation--opened');
+};
+
+/**
+ * @name buttonMenuClickHandler
+ * @description функция обработчик при нажатии на кнопку меню
+ */
+var buttonMenuClickHandler = function () {
+  changeNavigationClass();
+};
+
+/**
+ * @name buttonMenuPrsEnterHandler
+ * @description функция обработчик при нажатии на кнопку меню клавишей Enter
+ * @param {*} evt
+ */
+var buttonMenuPrsEnterHandler = function (evt) {
+  if (evt.keyCode === KEY_CODE_ENT) {
+    changeNavigationClass();
+  }
+};
+
+buttonMenu.addEventListener('click', buttonMenuClickHandler);
+buttonMenu.addEventListener('keydown', buttonMenuPrsEnterHandler);
