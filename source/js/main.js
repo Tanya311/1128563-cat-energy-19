126005c5187ee6612979448b98f1a83a5cbc515e
Tanya311 <55144291+Tanya311@users.noreply.github.com>
2020-04-26 19:21:55 +0300

    Обернули код JS в IIFE

--- source/js/main.js
+++ source/js/main.js
@@ -1,36 +1,40 @@
-var KEY_CODE_ENT = 25;
-var navigation = document.querySelector('.navigation')
-var buttonMenu = navigation.querySelector('.navigation__toggle');
-var navigationWrapper = navigation.querySelector('.navigation__wrapper');
+// Файл main.js
+'use strict';
+(function () {
+  var KEY_CODE_ENT = 25;
+  var navigation = document.querySelector('.navigation')
+  var buttonMenu = navigation.querySelector('.navigation__toggle');
+  var navigationWrapper = navigation.querySelector('.navigation__wrapper');
 
-navigation.classList.remove('navigation--nojs');
+  navigation.classList.remove('navigation--nojs');
 
-/**
- * @name changeNavigationClass
- * @description функция переключения класса навигации
- */
-var changeNavigationClass = function () {
-  navigation.classList.toggle('navigation--opened');
-};
+  /**
+  * @name changeNavigationClass
+  * @description функция переключения класса навигации
+  */
+  var changeNavigationClass = function () {
+    navigation.classList.toggle('navigation--opened');
+  };
 
-/**
- * @name buttonMenuClickHandler
- * @description функция обработчик при нажатии на кнопку меню
- */
-var buttonMenuClickHandler = function () {
-  changeNavigationClass();
-};
-
-/**
- * @name buttonMenuPrsEnterHandler
- * @description функция обработчик при нажатии на кнопку меню клавишей Enter
- * @param {*} evt
- */
-var buttonMenuPrsEnterHandler = function (evt) {
-  if (evt.keyCode === KEY_CODE_ENT) {
+  /**
+  * @name buttonMenuClickHandler
+  * @description функция обработчик при нажатии на кнопку меню
+  */
+  var buttonMenuClickHandler = function () {
     changeNavigationClass();
-  }
-};
+  };
 
-buttonMenu.addEventListener('click', buttonMenuClickHandler);
-buttonMenu.addEventListener('keydown', buttonMenuPrsEnterHandler);
+  /**
+  * @name buttonMenuPrsEnterHandler
+  * @description функция обработчик при нажатии на кнопку меню клавишей Enter
+  * @param {*} evt
+  */
+  var buttonMenuPrsEnterHandler = function (evt) {
+    if (evt.keyCode === KEY_CODE_ENT) {
+      changeNavigationClass();
+    }
+  };
+
+  buttonMenu.addEventListener('click', buttonMenuClickHandler);
+  buttonMenu.addEventListener('keydown', buttonMenuPrsEnterHandler);
+})();
