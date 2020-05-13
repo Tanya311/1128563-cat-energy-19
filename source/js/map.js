a5d161a5f3d4ef43932d63d65391ddad66ffd5d3
Tanya311 <55144291+Tanya311@users.noreply.github.com>
2020-05-13 15:05:09 +0300

    Исправление ошибок после проыерки

--- source/js/map.js
+++ source/js/map.js
@@ -1,20 +1,17 @@
+'use strict'
 var myMap;
       ymaps.ready(init); // Ожидание загрузки API с сервера Яндекса
       function init () {
         myMap = new ymaps.Map("map", {
-          center: [59.93869121515712,30.323054950139095], // Координаты центра карты
+          center: [59.938816, 30.323167], // Координаты центра карты
           zoom: 16 // Zoom
         });
 
-        var myPlacemark = new ymaps.Placemark([59.93869121515712,30.323054950139095], {
-        balloonContentHeader: 'Приглашаем к сотрудничеству дилеров!',
-        balloonContentBody: 'г. Санкт-Петербург',
-        balloonContentFooter: 'ул. Большая Конюшенная, д. 19/8',
-    }, {
+        var myPlacemark = new ymaps.Placemark([59.938816, 30.323167], {
         iconLayout: 'default#image',
         iconImageHref: 'img/map-pin.png',
-        iconImageSize: [55, 50],
-        iconImageOffset: [-25, -50]
+        iconImageSize: [113, 106],
+        iconImageOffset: [-60.5, -106]
     });
 
     // Размещение геообъекта на карте.
