3daaa5e88a424949dbd324fd80b7288cc4118a61
Tanya311 <55144291+Tanya311@users.noreply.github.com>
2020-05-15 19:35:21 +0300

    Проверка на кроссбраузерность, БЭМа, и идентичное форматирование, фикс структуры кода

--- source/js/map.js
+++ source/js/map.js
@@ -1,4 +1,4 @@
-'use strict'
+"use strict"
 var myMap;
   ymaps.ready(init); // Ожидание загрузки API с сервера Яндекса
   function getWindowWidth() {
@@ -20,23 +20,23 @@ var myMap;
 
     if (getWindowWidth() < 768) {
     var myPlacemark = new ymaps.Placemark([59.938816, 30.323167], {
-      balloonContentHeader: 'Приглашаем к сотрудничеству дилеров!',
-      balloonContentBody: 'г. Санкт-Петербург',
-      balloonContentFooter: 'ул. Большая Конюшенная, д. 19/8',
+      balloonContentHeader: "Приглашаем к сотрудничеству дилеров!",
+      balloonContentBody: "г. Санкт-Петербург",
+      balloonContentFooter: "ул. Большая Конюшенная, д. 19/8",
     }, {
-      iconLayout: 'default#image',
-      iconImageHref: 'img/map-pin.png',
+      iconLayout: "default#image",
+      iconImageHref: "img/map-pin.png",
       iconImageSize: [55, 53],
       iconImageOffset: [-27.5, -53]
     });
     } else {
       var myPlacemark = new ymaps.Placemark([59.938816, 30.323167], {
-        balloonContentHeader: 'Приглашаем к сотрудничеству дилеров!',
-        balloonContentBody: 'г. Санкт-Петербург',
-        balloonContentFooter: 'ул. Большая Конюшенная, д. 19/8',
+        balloonContentHeader: "Приглашаем к сотрудничеству дилеров!",
+        balloonContentBody: "г. Санкт-Петербург",
+        balloonContentFooter: "ул. Большая Конюшенная, д. 19/8",
       }, {
-        iconLayout: 'default#image',
-        iconImageHref: 'img/map-pin.png',
+        iconLayout: "default#image",
+        iconImageHref: "img/map-pin.png",
         iconImageSize: [113, 106],
         iconImageOffset: [-60.5, -106]
       });
